4fbbcf112ba46429cd43a1d4b26d8cb6fe79ef5e
Khaled Mohsen <pres.kbayomy@gmail.com>
2023-04-30 00:00:26 +0300

    Test

--- lib/src/schema/opf/epub_language_related_attributes.dart
+++ lib/src/schema/opf/epub_language_related_attributes.dart
@@ -1,14 +1,14 @@
 class EpubLanguageRelatedAttributes {
   /// https://www.w3.org/TR/epub-33/#attrdef-xml-lang
-  final String? XmlLang;
+  String? XmlLang;
 
   /// Paragraph direction/Global base direction. (v3.3)
   ///
   /// Reading systems will assume the value "auto" when EPUB creators omit the attribute or use an invalid value.
   /// https://www.w3.org/TR/epub-33/#attrdef-dir
-  final String? Dir;
+  String? Dir;
 
-  const EpubLanguageRelatedAttributes({
+  EpubLanguageRelatedAttributes({
     this.XmlLang,
     this.Dir,
   });
