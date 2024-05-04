6aa13c4a2da2f8f8d80e9c6c1a69680fc952e17d
Alana Gilston <abluescarab@users.noreply.github.com>
2024-05-04 02:50:39 -0700

    Fix innerText reference

--- lib/src/readers/package_reader.dart
+++ lib/src/readers/package_reader.dart
@@ -172,7 +172,7 @@ class PackageReader {
     metadataNode.children
         .whereType<XmlElement>()
         .forEach((XmlElement metadataItemNode) {
-      var innerText = metadataItemNode.value!;
+      var innerText = metadataItemNode.innerText;
       switch (metadataItemNode.name.local.toLowerCase()) {
         case 'title':
           result.titles!.add(
