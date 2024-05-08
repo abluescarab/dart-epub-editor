1e2c23484ab0bf8db77402dc0ba50fd4f4d7e757
Alana Gilston <abluescarab@users.noreply.github.com>
2024-05-07 23:04:13 -0700

    Add ID to EpubMetadataDate

--- lib/src/schema/opf/epub_metadata_date.dart
+++ lib/src/schema/opf/epub_metadata_date.dart
@@ -1,16 +1,17 @@
 import 'package:quiver/core.dart';
 
 class EpubMetadataDate {
+  String? id;
   String? date;
   String? event;
 
   @override
-  int get hashCode => hash2(date.hashCode, event.hashCode);
+  int get hashCode => hash3(id.hashCode, date.hashCode, event.hashCode);
 
   @override
   bool operator ==(other) {
     var otherAs = other as EpubMetadataDate?;
     if (otherAs == null) return false;
-    return date == otherAs.date && event == otherAs.event;
+    return id == otherAs.id && date == otherAs.date && event == otherAs.event;
   }
 }
