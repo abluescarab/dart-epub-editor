3b9900029f1b2fc537167d6acf75a841f5852d97
Alana Gilston <abluescarab@users.noreply.github.com>
2024-05-20 13:54:06 -0700

    Fix null check error

--- lib/src/entities/epub_book.dart
+++ lib/src/entities/epub_book.dart
@@ -41,8 +41,8 @@ class EpubBook {
         collections.listsEqual(authorList, other.authorList) &&
         schema == other.schema &&
         content == other.content &&
-        collections.listsEqual(coverImage!.getContentStream(),
-            other.coverImage!.getContentStream()) &&
+        collections.listsEqual(coverImage?.getContentStream(),
+            other.coverImage?.getContentStream()) &&
         collections.listsEqual(chapters, other.chapters);
   }
 }
